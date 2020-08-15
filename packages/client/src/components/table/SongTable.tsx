ce0fde8a44b38c29af75dae7bb17399f72683040
Luke Prananta <m.l.r.prananta@student.tudelft.nl>
2020-08-15 17:08:12 +0200

    Resize artist pictures

--- packages/client/src/components/table/SongTable.tsx
+++ packages/client/src/components/table/SongTable.tsx
@@ -6,36 +6,22 @@ import { ArtistSongColumn } from './ArtistSongColumn'
 import { Container, Row as BRow } from 'react-bootstrap'
 
 interface Props {
-  // endpoint: Endpoint.RecentlyPlayed | Endpoint.TopTracks
-  // options: object
   tracks: Track[]
 }
 
 export const SongTable: React.FC<Props> = (props) => {
   const tracks = props.tracks
 
-  // const tracks = useApiRequest<{ items: Track[] }>(
-  //   props.endpoint,
-  //   props.options,
-  // ) || { items: [] }
-
   return (
     <Container fluid>
       <BRow className="auto">
         <GenericTable>
-          {/* <thead>
-        <th></th>
-        <th>Track</th>
-        <th>Artist</th>
-      </thead> */}
           <Body>
             {tracks.map((track, index) => (
               <Row key={track.name + index}>
                 <td>{index + 1}</td>
                 <AlbumArtColumn track={track} />
                 <ArtistSongColumn data={track} />
-                {/* <Column data={track} dataKey={'name'} />
-            <ArtistColumn data={track}></ArtistColumn> */}
               </Row>
             ))}
           </Body>
