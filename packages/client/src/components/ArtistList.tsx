c18b1636e74867d8ae95ecb0793d5dbb313e69f8
Luke Prananta <m.l.r.prananta@student.tudelft.nl>
2020-08-15 14:48:06 +0200

    Update artist list design

--- packages/client/src/components/ArtistList.tsx
+++ packages/client/src/components/ArtistList.tsx
@@ -10,24 +10,26 @@ export const ArtistList: React.FC<Props> = (props) => {
   const artists = props.artists
 
   return (
-    <Fragment>
-      <div className="ram">
-        {artists.map((item, index) => {
-          const image = item.images[0] ? item.images[0].url : ''
-          const artist = item.name
-          return (
-            <div key={item.id} className="ram-container">
-              <img src={image} alt={artist} className="ram-img"></img>
-              <div className="font-weight-normal artist-counter parent">
+    <div className="ram">
+      {artists.map((item, index) => {
+        const image = item.images[0] ? item.images[0].url : ''
+        const artist = item.name
+        return (
+          <div key={item.id} className="ram-container">
+            <img src={image} alt={artist} className="ram-img"></img>
+            {/* <div className="font-weight-normal artist-counter parent">
                 {index + 1}
-              </div>
-              <div className="font-weight-light artist-name truncate">
-                {artist}
-              </div>
+              </div> */}
+            <div className="font-weight-light artist-name truncate">
+              {artist}
             </div>
-          )
-        })}
-      </div>
-    </Fragment>
+          </div>
+        )
+      })}
+      {/* <div className="ram-container">
+        <img src={'filler.png'} alt="" className="ram-img"></img>
+        <div className="ram-filler"></div>
+      </div> */}
+    </div>
   )
 }
