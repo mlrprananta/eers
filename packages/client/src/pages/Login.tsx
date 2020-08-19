f1ee6d8fbf4c3019aec191528c2e2cbdf067dd5e
Luke Prananta <m.l.r.prananta@student.tudelft.nl>
2020-08-19 13:55:22 +0200

    Center display text

--- packages/client/src/pages/Login.tsx
+++ packages/client/src/pages/Login.tsx
@@ -9,7 +9,7 @@ export const Login: React.FC = () => {
     // <Jumbotron>
     <Container fluid className="full parent bg-primary">
       <div className="parent">
-        <h1 className="display-3 text-light">Eyes and ears</h1>
+        <h1 className="display-3 text-light text-center">Eers</h1>
         <p className="text-light">Connect with Spotify and get started!</p>
         <p>
           <Button onClick={authenticate} variant="outline-light">
